0ecb654b400a70d564194830e8439876858173f4
Aripov-Sirojiddin <aripov.2@osu.edu>
2024-09-27 17:26:47 -0400

    Creating a user with form_with

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }
 end
